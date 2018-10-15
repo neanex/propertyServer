4d74c7435f7432725d43bb1d03e97ad6cbfc0162
Nick Ooms <nick.ooms@neanex.com>
2018-10-15 18:11:39 +0200

    Added forge-token-multi-credentials

---
+++ server/forge-token-multi-credentials.js
@@ -0,0 +1,14 @@
+const multiAuth = {};
+
+module.exports = {
+    refresh: ({ clientId, urn, token }) => {
+        if (!multiAuth[urn]) {
+            multiAuth[urn] = new AuthClient(clientId);
+        }
+        const auth = multiAuth[urn];
+        auth.setCredentials({ access_token: token });
+        return auth;
+    },
+
+    get: urn => multiAuth[urn],
+};
