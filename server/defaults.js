1655c1741857c37fbdf7d2721e1a48f6ff4baa9e
Nick Ooms <nick.ooms@neanex.com>
2018-10-15 12:49:46 +0200

    Added port to defaults

--- server/defaults.js
+++ server/defaults.js
@@ -2,7 +2,8 @@
 
 var defaults = {
 	FORGE_CLIENT_ID: '<replace with your consumer key>',
-	FORGE_CLIENT_SECRET: '<replace with your consumer secret>'
+	FORGE_CLIENT_SECRET: '<replace with your consumer secret>',
+	PORT: 80
 };
 
 module.exports = defaults;
