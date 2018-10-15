090f614dd8f67e526e8c9a378d756e1ca6a38971
Nick Ooms <nick.ooms@neanex.com>
2018-10-15 12:01:07 +0200

    Created defaults.js to store placeholders in config.js

---
+++ server/defaults.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var FORGE_CLIENT_ID = '<replace with your consumer key>';
+var FORGE_CLIENT_SECRET = '<replace with your consumer secret>';
+
+var DEFAULTS = {
+	FORGE_CLIENT_ID,
+	FORGE_CLIENT_SECRET
+};
+
+module.exports = DEFAULTS;
