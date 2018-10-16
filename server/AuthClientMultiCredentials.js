26eada7dc7f1317f9c0b89f674336a48ea57ccd4
Nick Ooms <nick.ooms@neanex.com>
2018-10-16 15:58:41 +0200

    - Removed authName - Changed !! to Boolean() - Changed new Promise to Promise.resolve - Placed tokenUrl inside class - Placed scope inside class - Placed scopes inside class

--- server/AuthClientMultiCredentials.js
+++ server/AuthClientMultiCredentials.js
@@ -1,36 +1,15 @@
 const OAuth2 = require('../node_modules/forge-apis/src/auth/OAuth2');
 
-const AUTH_NAME = 'oauth2_application';
-
-const TOKEN_URL = '/authentication/v1/authenticate';
-
-const SCOPES = {
-    'data:read': 'The application will be able to read the end user’s data within the Autodesk ecosystem.',
-    'data:write': 'The application will be able to create, update, and delete data on behalf of the end user within the Autodesk ecosystem.',
-    'data:create': 'The application will be able to create data on behalf of the end user within the Autodesk ecosystem.',
-    'data:search': 'The application will be able to search the end user’s data within the Autodesk ecosystem.',
-    'bucket:create': 'The application will be able to create an OSS bucket it will own.',
-    'bucket:read': 'The application will be able to read the metadata and list contents for OSS buckets that it has access to.',
-    'bucket:update': 'The application will be able to set permissions and entitlements for OSS buckets that it has permission to modify.',
-    'bucket:delete': 'The application will be able to delete a bucket that it has permission to delete.',
-    'code:all': 'The application will be able to author and execute code on behalf of the end user (e.g., scripts processed by the Design Automation API).',
-    'account:read': 'For Product APIs, the application will be able to read the account data the end user has entitlements to.',
-    'account:write': 'For Product APIs, the application will be able to update the account data the end user has entitlements to.',
-    'user-profile:read': 'The application will be able to read the end user’s profile data.',
-    'viewables:read': 'The application will have read access to viewable resources such as thumbnails. This scope is a subset of data:read.'
-};
-
-const SCOPE = [
-    'data:write',
-    'data:read',
-    'bucket:read',
-    'bucket:create',
-    'bucket:delete'
-];
-
 class OAuth2MultiCredentials extends OAuth2 {
     constructor(clientId) {
-        super(clientId, null, SCOPE, false);
+        const scope = [
+            'data:write',
+            'data:read',
+            'bucket:read',
+            'bucket:create',
+            'bucket:delete'
+        ];
+        super(clientId, null, scope, false);
         Object.assign(this, {
             setCredentials: this.setCredentials.bind(this),
             getCredentials: this.getCredentials.bind(this),
@@ -40,11 +19,24 @@ class OAuth2MultiCredentials extends OAuth2 {
     }
 
     get authentication() {
-        return { tokenUrl: TOKEN_URL, scopes: SCOPES };
-    }
-
-    get authName() {
-        return AUTH_NAME;
+        return {
+            tokenUrl: '/authentication/v1/authenticate',
+            scopes: {
+                'data:read': 'The application will be able to read the end user’s data within the Autodesk ecosystem.',
+                'data:write': 'The application will be able to create, update, and delete data on behalf of the end user within the Autodesk ecosystem.',
+                'data:create': 'The application will be able to create data on behalf of the end user within the Autodesk ecosystem.',
+                'data:search': 'The application will be able to search the end user’s data within the Autodesk ecosystem.',
+                'bucket:create': 'The application will be able to create an OSS bucket it will own.',
+                'bucket:read': 'The application will be able to read the metadata and list contents for OSS buckets that it has access to.',
+                'bucket:update': 'The application will be able to set permissions and entitlements for OSS buckets that it has permission to modify.',
+                'bucket:delete': 'The application will be able to delete a bucket that it has permission to delete.',
+                'code:all': 'The application will be able to author and execute code on behalf of the end user (e.g., scripts processed by the Design Automation API).',
+                'account:read': 'For Product APIs, the application will be able to read the account data the end user has entitlements to.',
+                'account:write': 'For Product APIs, the application will be able to update the account data the end user has entitlements to.',
+                'user-profile:read': 'The application will be able to read the end user’s profile data.',
+                'viewables:read': 'The application will have read access to viewable resources such as thumbnails. This scope is a subset of data:read.'
+            }
+        };
     }
 
     setCredentials(credentials) {
@@ -56,13 +48,11 @@ class OAuth2MultiCredentials extends OAuth2 {
     }
 
     isAuthorized() {
-        return !!this.credentials;
+        return Boolean(this.credentials);
     }
 
     authenticate() {
-        return new Promise((resolve) => {
-            resolve(this.getCredentials());
-        });
+        return Promise.resolve(this.getCredentials());
     }
 }
 
