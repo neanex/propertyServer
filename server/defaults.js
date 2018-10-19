781f86ae9446f8cb92d92612f7971905801aa25d
Nick Ooms <nick.ooms@neanex.com>
2018-10-19 10:28:56 +0200

    Added defaults

---
+++ server/defaults.js
@@ -0,0 +1,9 @@
+const defaults = {
+    // Replace placeholder below by the Consumer Key and Consumer Secret you got from
+    // http://developer.autodesk.com/ for the production server
+    FORGE_CLIENT_ID: '<replace with your consumer key>',
+    FORGE_CLIENT_SECRET: '<replace with your consumer secret>',
+    PORT: 80,
+};
+
+module.exports = defaults;
