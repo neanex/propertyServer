7d055f4d946ddddae9fe31f8054601b56ddfe038
Nick Ooms <nick.ooms@neanex.com>
2018-10-19 10:39:01 +0200

    - Do not log API key for multi-credentials setup - Use port from config

--- start.js
+++ start.js
@@ -17,22 +17,34 @@
 // Forge Property Server
 // by Cyrille Fauvel - Autodesk Developer Network (ADN)
 //
-'use strict';
+
+/* eslint no-console: 0 */
 
 // To avoid the EXDEV rename error, see http://stackoverflow.com/q/21071303/76173
 process.env.TMPDIR = 'tmp';
-//process.env ['NODE_TLS_REJECT_UNAUTHORIZED'] ='0' ; // Ignore 'UNABLE_TO_VERIFY_LEAF_SIGNATURE' authorization error
+// process.env ['NODE_TLS_REJECT_UNAUTHORIZED'] ='0' ;
+// Ignore 'UNABLE_TO_VERIFY_LEAF_SIGNATURE' authorization error
 
-var app = require('./server/server');
+const app = require('./server/server');
+const {
+    credentials: {
+        client_id: apiKey,
+        HAS_DEFAULTS,
+    },
+} = require('./server/config');
 
-var server = app.listen(app.get('port'), function () {
-	console.log('API key ' + process.env.FORGE_CLIENT_ID);
-	console.log('Server listening on port ' + server.address().port);
+const port = app.get('port');
+
+const server = app.listen(port, () => {
+    if (!HAS_DEFAULTS) {
+        console.log(`API key ${apiKey}`);
+    }
+    console.log(`Server listening on port ${port}`);
 });
 
-server.on('error', function (err) {
-	if (err.errno == 'EACCES') {
-		console.log('Port ' + app.get('port') + ' already in use.\nExiting...');
-		process.exit(1);
-	}
+server.on('error', ({ errno }) => {
+    if (errno === 'EACCES') {
+        console.log(`Port ${port} already in use.\nExiting...`);
+        process.exit(1);
+    }
 });
